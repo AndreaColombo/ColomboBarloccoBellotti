8c142162621cb78fdbfaf99975d1db520f451d8a
AndreaColombo <andrea.colombo.897@gmail.com>
2016-03-14 12:52:21 +0100

    cose

--- adc.cpp
+++ adc.cpp
@@ -8,25 +8,24 @@ Adc::Adc(){
 	RCC->AHB1ENR |= RCC_AHB1ENR_GPIOCEN;
 	RCC->APB1ENR |= RCC_APB2ENR_ADC1EN;
 	
-	ADC1->CR1 = 1<<3;
-	ADC1->CR1 |= 1<<1;
-	ADC1->CR1 |= 1<<1;
-	ADC1->CR2 |= ADC_CR2_ADON;
+	ADC1->CR1 = 1<<3 | 1<<1 | 1<<0 | 1<<8 |1<<24|1<<25;
 	
+	ADC1->CR2 |= ADC_CR2_ADON;
+	ADC1->CR2 |= 1<<1;
 	GPIOC->MODER |= 1<<2;
 	GPIOC->MODER |= 1<<3;
 	
 }
 
 void Adc::init(){
+	ADC1->CR1 = 1<<8;
 	return;
 }
 
 void Adc::start(){
 	//cr1 bit 8 scan
 	//cr2 bit 30 start
-	ADC1->CR1 = 1<<8;
-	ADC1->CR2 = 1<<30;
+	ADC1->CR2 = 1<<30; 
 }
 
 int Adc::read(){
